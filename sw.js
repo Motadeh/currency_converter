cb140ebf7fb6dea339b77e6953c935a823cdfadc
Tolulope Adeleye <adeleyetolu9250@gmail.com>
2018-07-01 17:03:05 +0100

    service worker

--- sw.js
+++ sw.js
@@ -1,20 +1,66 @@
-var filesToCache = [
-    '.',
-    'db.js',
-    'main.js',
-    'converter.js',
-    'currencyconverter.css',
-    'currencyconverter.html',
-    'idb/lib/idb.js',
+const staticCacheName = 'cache-v1';
 
+let allCaches = [
+    staticCacheName
+  ];
+
+  const urlsToCache = [
+    '/',
+    '/main.js',
+    '/db.js',
+    '/idb/lib/idb.js',
+    '/currencyconverter.html',
+    '/currencyconverter.css',
+    '/converter.js'
 ];
 
-var staticCacheName = 'cache-v1';
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
+    console.log('[ServiceWorker] install');
     event.waitUntil(
-        caches.open(staticCacheName).then(cache => {
-            return cache.addAll(filesToCache);
+        caches.open(staticCacheName)
+
+            .then(
+
+                (cache) => {
+                    console.log('[ServiceWorker] Caching app shell');
+                    return cache.addAll(urlsToCache);
+                }
+            )
+    )
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('[ServiceWorker] Activate');
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', cacheName);
+                    return cacheName.startsWith('currency-') &&
+                        !allCaches.includes(cacheName);
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
         })
     );
+})
+
+self.addEventListener('fetch', (event) => {
+    console.log('[ServiceWorker] Fetch', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) return response;
+            return fetch(event.request).then(function (response) {
+                // console.log('[ServiceWorker] Response', response);
+                return response
+            });
+        })
+    );
+})
+
+self.addEventListener('message', function (event) {
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
 });
