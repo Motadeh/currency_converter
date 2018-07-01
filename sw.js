eb7997e529011d3943123dd7fab7e1d3fb25d9fe
Tolulope Adeleye <adeleyetolu9250@gmail.com>
2018-07-01 19:45:44 +0100

    more

--- sw.js
+++ sw.js
@@ -59,20 +59,20 @@ self.addEventListener('activate', (event) => {
     );
 }) */
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function(event) {
     var dataUrl = 'https://free.currencyconverterapi.com/api/v5/convert?';
-    if (e.request.url.includes(dataUrl)) {
-      e.respondWith(
-          fetch(e.request).then( (res) => {
+    if (event.request.url.includes(dataUrl)) {
+      event.respondWith(
+          fetch(event.request).then( (res) => {
               console.log('Fetching from Network');
               const resClone = res.clone();
               caches.open(dynamicCacheName).then( (cache) => {
-                  cache.put(e.request, resClone);
+                  cache.put(event.request, resClone);
               });
               return res;
           }).catch(function() {
               console.log('Fetching from Cache');
-              return caches.match(e.request);
+              return caches.match(event.request);
           })
       );
     } else {
