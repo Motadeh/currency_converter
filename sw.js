6156ceb3f7dfa714f1602b40aceb8fccfb9e5eef
Tolulope Adeleye <adeleyetolu9250@gmail.com>
2018-07-01 18:54:12 +0100

    and more changes

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@ self.addEventListener('activate', (event) => {
     );
 })
 
-self.addEventListener('fetch', (event) => {
+/* self.addEventListener('fetch', (event) => {
     console.log('[ServiceWorker] Fetch', event.request.url);
     event.respondWith(
         caches.match(event.request).then(function (response) {
@@ -57,54 +57,36 @@ self.addEventListener('fetch', (event) => {
             });
         })
     );
-})
+}) */
 
 self.addEventListener('fetch', function(e) {
-
     var dataUrl = 'https://free.currencyconverterapi.com/api/v5/convert?';
-
     if (e.request.url.includes(dataUrl)) {
-  
       e.respondWith(
-  
           fetch(e.request).then( (res) => {
-  
               console.log('Fetching from Network');
-  
               const resClone = res.clone();
-  
               caches.open(dynamicCacheName).then( (cache) => {
-  
                   cache.put(e.request, resClone);
-  
               });
-  
               return res;
-  
           }).catch(function() {
-  
               console.log('Fetching from Cache');
-  
               return caches.match(e.request);
-  
           })
-  
       );
-  
     } else {
-  
-      e.respondWith(
-  
-        caches.match(e.request).then( (response) => {
-  
-          return response || fetch(e.request);
-  
-        })
-  
-      );
-  
+        console.log('[ServiceWorker] Fetch', event.request.url);
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                if (response) return response;
+                return fetch(event.request).then(function (response) {
+                    // console.log('[ServiceWorker] Response', response);
+                    return response
+                });
+            })
+        );
     }
-  
   });
 
 
